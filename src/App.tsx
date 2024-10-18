2c1852e579692e7cdab9ef8369566d91532ec039
METZ <metzo3232@gmail.com>
2024-10-18 11:38:23 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div className='bg-blue-500'>
+      hiii
+     </div>
+    </div>
+  );
+}
+
+export default App;
